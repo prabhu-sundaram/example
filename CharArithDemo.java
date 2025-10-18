1e9775d378f319a54cfb7d08dd2b56219af2bbf5
prabhu-sundaram <asprabhueee@gmail.com>
2025-10-18 19:16:24 +0530

    Reapply "Initial commit"
    This reverts commit 016d9fa587721b14a7b810a82726cbc16e8da0f3.

---
+++ CharArithDemo.java
@@ -0,0 +1,24 @@
+// Character variables can be handled like integers.
+class CharArithDemo {
+public static void main(String[] args) {
+char ch;
+ch = 'X';
+System.out.println("ch contains " + ch);
+ch++; // increment ch
+System.out.println("ch is now " + ch);
+ch = 90; // give ch the value Z
+System.out.println("ch is now " + ch);
+
+char ch1='a';
+char ch2='A';
+System.out.println("ch1"+ch1);
+System.out.println("ch2"+ch2);
+ch1++;
+ch2++;
+System.out.println("ch1"+ch1);
+System.out.println("ch2"+ch2);
+System.out.println(ch1);
+System.out.println(ch2);
+
+}
+}
