3418d086d5a717c6f77b4af31b11eca0047fff87
prabhu-sundaram <asprabhueee@gmail.com>
2025-10-18 18:32:58 +0530

    Initial commit

---
+++ test.java
@@ -0,0 +1,38 @@
+class test
+{
+    public static void main(String args[])
+    {
+        System.out.println("42423");
+System.out.println("prabhu");
+int a=10;
+int b=2;
+int c;
+System.out.println("a*b:"+a*b);
+
+if(a>b) System.out.println("a is greater than b");
+
+if(a>b){
+    System.out.println("a is greater than b");
+}
+else
+{
+       System.out.println("a is lesser than b");
+ 
+}
+c=a*b;
+System.out.println("a,b,c:"+a+":"+b+":"+c);
+int i;
+for(i=0;i<=5;i++)
+System.out.println("i:"+i);
+
+    switch(a)
+    case 1:
+    System.out.println("a is 1");
+    case 2:
+    System.out.println("a is 2");
+    case default:
+    System.out.println("a is others");
+    }
+
+
+}
