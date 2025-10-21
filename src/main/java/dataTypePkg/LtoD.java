7b9476b958a9e6e5c6bc27f325a0a31f52866968
prabhu-sundaram <asprabhueee@gmail.com>
2025-10-21 07:33:56 +0530

    program control

---
+++ src/main/java/dataTypePkg/LtoD.java
@@ -0,0 +1,15 @@
+package dataTypePkg;
+// Demonstrate automatic conversion from long to double.
+class LtoD {
+public static void main(String[] args) {
+long L;
+double D;
+L = 100123285L;
+D = L;
+
+//D = 100123285.0;
+//L = D; // Illegal!!!
+
+System.out.println("L and D: " + L + "\t" + D);
+}
+}
