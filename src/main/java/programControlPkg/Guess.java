869beb8d85aa191ef1d28f171e6925c8d94c763e
prabhu-sundaram <asprabhueee@gmail.com>
2025-10-22 05:04:26 +0530

    program control statement examples

--- src/main/java/programControlPkg/Guess.java
+++ src/main/java/programControlPkg/Guess.java
@@ -2,13 +2,21 @@ package programControlPkg;
 
 // Guess the letter game.
 class Guess {
-public static void main(String[] args)
-throws java.io.IOException {
+public static void main(String[] args) throws java.io.IOException {
 char ch, answer = 'K';
 System.out.println("I'm thinking of a letter between A and Z.");
 System.out.print("Can you guess it: ");
 ch = (char) System.in.read(); // read a char from the keyboard
 System.out.println("Your guess is: " + ch);
 if(ch == answer) System.out.println("** Right **");
+else System.out.println("...Sorry, you're wrong.");
+
+if(ch == answer) System.out.println("** Right **");
+else {
+System.out.print("...Sorry, you're ");
+// a nested if
+if(ch < answer) System.out.println("too low");
+else System.out.println("too high");
+}
 }
 }
--- src/main/java/programControlPkg/KbIn.java
+++ src/main/java/programControlPkg/KbIn.java
@@ -2,8 +2,7 @@ package programControlPkg;
 
 // Read a character from the keyboard.
 class KbIn {
-public static void main(String[] args)
-throws java.io.IOException {
+public static void main(String[] args) throws java.io.IOException {
 char ch;
 System.out.print("Press a key followed by ENTER: ");
 ch = (char) System.in.read(); // get a char
