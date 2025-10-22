ec7ec9d52d253455840ed76ed87e47a691077b6a
prabhu-sundaram <asprabhueee@gmail.com>
2025-10-22 09:51:07 +0530

    class examples

---
+++ src/main/java/classExamplesPkg/Vehicle.java
@@ -0,0 +1,8 @@
+package classExamplesPkg;
+
+// A program that uses the Vehicle class.
+class Vehicle {
+int passengers; // number of passengers
+int fuelcap; // fuel capacity in gallons
+int mpg; // fuel consumption in miles per gallon
+}
