ab04b4ed45ed675be017902bd5aa67f0189fe8ed
prabhu-sundaram <asprabhueee@gmail.com>
2025-10-25 09:25:25 +0530

    array examples

---
+++ src/main/java/dataTypePkg/arrayPkg/ArrayDemo.java
@@ -0,0 +1,16 @@
+package dataTypePkg;
+
+// Demonstrate a one-dimensional array.
+class ArrayDemo {
+public static void main(String[] args) {
+int[] sample = new int[10];
+int i;
+
+for(i = 0; i < 10; i = i+1)
+sample[i] = i*2;
+
+for(i = 0; i < 10; i = i+1)
+System.out.println("This is sample[" + i + "]: " +
+sample[i]);
+}
+}
---
+++ src/main/java/dataTypePkg/arrayPkg/ArrayErr.java
@@ -0,0 +1,15 @@
+package dataTypePkg;
+
+// Demonstrate an array overrun.
+class ArrayErr {
+public static void main(String[] args) {
+int[] sample = new int[10];
+int i;
+// generate an array overrun
+for(i = 0; i < 100; i = i+1)
+{
+sample[i] = i;
+System.out.println(i);
+}
+}
+}
